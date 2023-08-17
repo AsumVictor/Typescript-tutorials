c53738d024d201173d7bf806465da568921f5560
AsumVictor <Iamasum369@gmail.com>
2023-08-17 22:53:55 +0000

    Objects and arrays

--- build/js/main.js
+++ build/js/main.js
@@ -37,3 +37,54 @@ const has = (a) => {
     return [a % 10];
 };
 //  Objects, Arrays, Tuples & Enums
+/*
+To declare and array use [] at the end of the data type
+eg.
+array of strings, let str: string[] = []
+array of numbers, let str: number[] = []
+array of numbers and string, let str: (number | number)[] = []
+
+*/
+let months = [];
+//
+// months.push(3); Error occured
+months.push("feb");
+// TUBLE is an array with fixed arrangment and length
+// Tuble is more restricted than arrays
+// Declare tuble with arrays of data types
+// Example let make question
+let question = [1, "What is typescript", true];
+// This tuble contains fixed length of 3 and fixed positions
+question.push("scsc");
+console.log(question);
+// Now I have descript student
+// let student: Student = {}
+/*
+error TS2739: Type '{}' is missing the following properties from type 'Student': fullname, age, isActive, hasCar, books
+
+92 let student: Student = {}
+      ~~~~~~~
+*/
+let std = { fullname: 'string',
+    age: 8,
+    isActive: false,
+    hasCar: 0,
+    books: ['book', 7],
+};
+// Making hasCar property optional
+// Lets user this in a function
+const teacher = {
+    fullname: 'James Ama',
+    age: 39,
+    isActive: false,
+};
+const teacher2 = {
+    age: 39,
+    isActive: false,
+};
+// Lets make a function that takes optional parameter
+const greet = (teacher) => {
+    console.log(`Hello Mr. ${teacher.fullname}`);
+};
+greet(teacher2);
+;
--- src/main.ts
+++ src/main.ts
@@ -46,3 +46,99 @@ const has = (a: number) => {
 };
 
 //  Objects, Arrays, Tuples & Enums
+/*
+To declare and array use [] at the end of the data type
+eg. 
+array of strings, let str: string[] = []
+array of numbers, let str: number[] = []
+array of numbers and string, let str: (number | number)[] = []
+
+*/
+
+let months: string[] = [];
+
+//
+// months.push(3); Error occured
+months.push("feb");
+
+// TUBLE is an array with fixed arrangment and length
+// Tuble is more restricted than arrays
+
+// Declare tuble with arrays of data types
+// Example let make question
+
+let question: [number, string, boolean] = [1, "What is typescript", true];
+
+// This tuble contains fixed length of 3 and fixed positions
+
+question.push("scsc");
+console.log(question);
+
+// question = [1,1, false]
+// src/main.ts:76:15 - error TS2322: Type 'number' is not assignable to type 'string'.
+// 76 question = [1,1, false]
+
+// Objects
+// user type to make the object
+type Student = {
+  fullname: string;
+  age: number;
+  isActive: boolean;
+  hasCar: number | boolean;
+  books: (string | number)[];
+};
+
+// Now I have descript student
+ // let student: Student = {}
+ /*
+ error TS2739: Type '{}' is missing the following properties from type 'Student': fullname, age, isActive, hasCar, books
+
+92 let student: Student = {}
+       ~~~~~~~
+ */
+
+  let std: Student = {fullname: 'string',
+  age: 8,
+  isActive: false,
+  hasCar:0,
+  books: ['book', 7],
+}
+
+// Making a propert optional
+type Teacher = {
+  fullname?: string;
+  age: number;
+  isActive: boolean;
+  hasCar?: number | boolean;
+};
+
+// Making hasCar property optional
+
+// Lets user this in a function
+
+const teacher: Teacher = {
+  fullname: 'James Ama',
+  age: 39,
+  isActive: false,
+}
+
+const teacher2: Teacher = {
+  age: 39,
+  isActive: false,
+}
+
+// Lets make a function that takes optional parameter
+const greet = (teacher: Teacher) => {
+  console.log(`Hello Mr. ${teacher.fullname}`)
+}
+
+greet(teacher2)
+
+// Interface works same as type but lil 
+interface Mentor {
+  fullname?: string;
+  age: number;
+  isActive: boolean;
+  hasCar?: number | boolean;
+};
+
